2453a55a1f9a3594310d4f766d089522a25b0ee0
kjw2300 <kjw2300@alten-korea.com>
2024-11-21 11:25:53 +0900

    material-UI

--- src/App.js
+++ src/App.js
@@ -1,44 +1,70 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Customer from './Components/Customer';
-import { render } from '@testing-library/react';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
 
-const customers =[ 
-  {
-  'id': '1',
-  'image': 'https://dummyimage.com/64/64/1',
-  'name': '홍길동',
-  'birthDay': '961222',
-  'gender': '남',
-  'job': '대학생'
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
   },
+  table: {
+    minWidth: 1080,
+  },
+});
 
+const customers = [
   {
-    'id': '2',
-    'image': 'https://dummyimage.com/64/64/2',
-    'name': '홍길동1',
-    'birthDay': '961222',
-    'gender': '남',
-    'job': '대학생'
-    },
+    id: '1',
+    image: 'https://dummyimage.com/64/555555/ffffff',
+    name: '홍길동',
+    birthDay: '961222',
+    gender: '남',
+    job: '대학생',
+  },
+  {
+    id: '2',
+    image: 'https://dummyimage.com/64/777777/ffffff',
+    name: '홍길동1',
+    birthDay: '961223',
+    gender: '남',
+    job: '대학생',
+  },
+  {
+    id: '3',
+    image: 'https://dummyimage.com/64/111111/ffffff',
+    name: '홍길동2',
+    birthDay: '961224',
+    gender: '남',
+    job: '대학생',
+  },
+];
 
-    {
-      'id': '3',
-      'image': 'https://dummyimage.com/64/64/3',
-      'name': '홍길동2',
-      'birthDay': '961222',
-      'gender': '남',
-      'job': '대학생'
-      }
-]
-
-function App() {
-  
+class App extends Component {
+  render() {
+    const { classes } = this.props; // props에서 classes 추출
     return (
-      <> 
-      {
-          customers.map((customer) => {
-            return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(customer => (
               <Customer
                 key={customer.id}
                 id={customer.id}
@@ -48,14 +74,12 @@ function App() {
                 gender={customer.gender}
                 job={customer.job}
               />
-            )
-          })
-        }
-   
-      </>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
     );
-  
- 
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
