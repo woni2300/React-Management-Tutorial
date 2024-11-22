8f68680c5fa119548748d605c165b595aa1ce40d
kjw2300 <kjw2300@alten-korea.com>
2024-11-22 11:53:56 +0900

    add

--- client/src/App.js
+++ client/src/App.js
@@ -8,18 +8,35 @@ import TableBody from '@material-ui/core/TableBody';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import { withStyles } from '@material-ui/core/styles';
+import { alpha, withStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import fade from '@material-ui/core/styles/colorManipulator';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+
 
 const styles = theme => ({
   root: {
     width: '100%',
-    overflowX: 'auto',
-    display: 'flex',
-    justifyContent: 'center',
+    minWidth:1080,
   },
-  table: {
-    maxWidth: 1080,
+  paper:{
+    marginLeft:18,
+    marginRight:18
+  },
+  menu:{
+    marginTop:15,
+    marginBottom:15,
+    display:'flex',
+    justifyContent:'center',
+  },
+  table: {    
     minWidth: 1080,
   },
   tableHead: {
@@ -37,12 +54,58 @@ const styles = theme => ({
   },
   progress: {
     margin: theme.spacing(2)
-  }
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
 });
 
-const colProperties = [
-  "번호", "이미지", "이름", "생년월일", "성별", "직업","설정"
-]
 
 /**
  * 1) constructor ()
@@ -96,15 +159,52 @@ class App extends Component {
 
   render() {
     const { classes } = this.props; // props에서 classes 추출
+    const colProperties = [
+      "번호", "이미지", "이름", "생년월일", "성별", "직업", "설정"
+    ]
+    
     return (
-      <>
-        <Paper className={classes.root}>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" noWrap>
+              고객관리 시스템
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="검색하기..."
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+        <div className={classes.menu} >
+          <CustomerAdd stateRefresh={this.stateRefresh} />
+
+        </div>
+        <Paper className={classes.paper} >
+
           <Table className={classes.table}>
             <TableHead>
               <TableRow>
                 {
                   colProperties.map(col => {
-                    return <TableCell className={classes.tableHead}>{col}</TableCell>
+                    return <TableCell key={col} className={classes.tableHead}>{col}</TableCell>
                   })
                 }
               </TableRow>
@@ -130,9 +230,9 @@ class App extends Component {
             </TableBody>
           </Table>
         </Paper>
-        <CustomerAdd stateRefresh={this.stateRefresh} />
+        
 
-      </>
+      </div>
 
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  typography:{
+    fontFamily:'"Noto Sans KR", serif'
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <MuiThemeProvider theme={theme}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
+  </MuiThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
